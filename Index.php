02995dceab1b120ee08e19861e8b3eb739310f4b
nabila712 <nabilaziyan7@gmail.com>
2021-10-16 16:20:39 +0700

    Mengedit file index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>ziyan nabila(205410128)</h3>"
+echo "<h3>Ziyan nabila(205410128)</h3>"
 
 ?>
